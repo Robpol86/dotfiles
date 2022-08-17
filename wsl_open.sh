a8dfb58f8d09a6785fbf08a25fb74462f3f3ce05
Robpol86 <robpol86@gmail.com>
2022-08-17 14:13:15 -0500

    Adding "open" command for WSL. (#4)
    * TODO
    
    * Adding "open" command for WSL.
    
    Scoped functionality similar to the macOS tool.
    
    * first draft
    
    * open
    
    * Done

---
+++ wsl_open.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+
+# Usage: open [-R] FILE_DIR_OR_LINK
+#        open -h
+#
+# Run "open -R ..." to reveal the file in explorer instead of opening it.
+
+set -e  # Exit script if a command fails.
+set -u  # Treat unset variables as errors and exit immediately.
+
+usage() {
+    echo "Usage: open [-R] FILE_DIR_OR_LINK" >&2
+    echo "       open -h" >&2
+    echo 'Run "open -R ..." to reveal the file in explorer instead of opening it.' >&2
+    exit 2
+}
+
+# Handle -R.
+case ${1:-} in
+    -h)
+        usage
+        ;;
+    -R|-r)
+        REVEAL=true
+        shift
+        ;;
+    *)
+        REVEAL=
+esac
+
+# Usage on no arguments.
+if [ $# -eq 0 ]; then
+    usage
+fi
+
+# Reveal in explorer.
+if [ "$REVEAL" = true ]; then
+    windows_path="$(wslpath -w "$*")"
+    /mnt/c/Windows/explorer.exe "/select," "$windows_path" || true  # TODO https://github.com/microsoft/WSL/issues/6565
+    exit 0
+fi
+
+# Open file/link with https://github.com/wslutilities/wslu.
+wslview "$*"
